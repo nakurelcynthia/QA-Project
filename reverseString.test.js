d410311ee0e5bafb2b1a299ef5051a22a78e5a27
birhanu <birhanutbelay@gmail.com>
2025-03-27 09:07:26 +0300

    Tested a string reversing

---
+++ reverseString.test.js
@@ -0,0 +1,7 @@
+const reverseString = require('./stringUtils');
+test('reverse a string',() =>{
+    expect(reverseString('hello')).toBe('olleh');
+    expect(reverseString('')).toBe('');
+    expect(reverseString('a')).toBe('a');
+    
+});
